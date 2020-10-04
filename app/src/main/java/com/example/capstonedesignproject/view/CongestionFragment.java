f7873dc591b86556f0206b9c71f99f95c3785fe5
aservmz@naver.com <aservmz@naver.com>
2020-10-04 22:56:16 +0900

    [MAP API] 카카오 맵 API 적용

--- app/src/main/java/com/example/capstonedesignproject/view/CongestionFragment.java
+++ app/src/main/java/com/example/capstonedesignproject/view/CongestionFragment.java
@@ -10,56 +10,29 @@ import android.view.ViewGroup;
 
 import com.example.capstonedesignproject.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CongestionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import net.daum.mf.map.api.MapView;
+
+
 public class CongestionFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public CongestionFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CongestionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CongestionFragment newInstance(String param1, String param2) {
-        CongestionFragment fragment = new CongestionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_congestion, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_congestion, container, false);
+
+        MapView mapView = new MapView(getContext());
+
+        ViewGroup mapViewContainer = v.findViewById(R.id.mapView);
+        mapViewContainer.addView(mapView);
+
+        return v;
     }
 }
--- app/src/main/java/com/example/capstonedesignproject/view/LoginActivity.java
+++ app/src/main/java/com/example/capstonedesignproject/view/LoginActivity.java
@@ -2,11 +2,20 @@ package com.example.capstonedesignproject.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
 import com.example.capstonedesignproject.R;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
         // TODO 이메일, 비밀번호 입력 안하면 로그인 버튼 비활성화
         // TODO 로그인 작업 처리 중 프로그레스 바 띄우기
         // TODO 카카오톡, 구글, 페이스북으로 로그인하기 추가
+        Log.d("Debug Key", getSigneture(getApplicationContext()));
 
     }
 
@@ -38,5 +48,28 @@ public class LoginActivity extends AppCompatActivity {
         // TODO 비밀번호찾기 페이지로 이동
         Intent intent = new Intent(this, FindPasswordActivity.class);
         startActivity(intent);
+    }
+
+    public static String getSigneture(Context context){
+        PackageManager pm = context.getPackageManager();
+        try{
+            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+
+            for(int i = 0; i < packageInfo.signatures.length; i++){
+                Signature signature = packageInfo.signatures[i];
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    return Base64.encodeToString(md.digest(), Base64.NO_WRAP);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }catch(PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
